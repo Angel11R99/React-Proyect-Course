6aabd22c888a9308028a41c5041b55d26e2920e2
Angel11R99 <angelrodriguez11r99@gmail.com>
2025-06-08 10:10:46 -0400

    :memo: Template de Strings (Concatenaciones)

---
+++ Introduccion a JS/js/template-string.js
@@ -0,0 +1,8 @@
+// Template de Strings y Concatenación
+
+const nombre = 'Angel';
+const edad = 25;
+const nacionalidad = 'Dominicano';
+
+// Concatenación
+console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años y soy ${nacionalidad}`);
