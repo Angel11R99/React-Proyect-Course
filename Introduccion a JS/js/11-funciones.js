bb6df471f8b67efeda256d31a62e1a821a8aacd4
Angel11R99 <angelrodriguez11r99@gmail.com>
2025-06-10 12:43:59 -0400

    :package: Introduccion a las Funciones

---
+++ Introduccion a JS/js/11-funciones.js
@@ -0,0 +1,13 @@
+// Function Declaration
+
+/* 
+Nota:
+- NaN => Not A Number.
+- Las funciones se le asignan parametros `valor1, valor2` y lo que se le envia son los argumentos `sumar(12, 10)`. 
+- Parametros por default `valor1 = 0, valor2 = 0` 
+*/
+function sumar (valor1 = 0, valor2 = 0) {
+    console.log(valor1 + valor2);
+}
+
+sumar(12, 10)
