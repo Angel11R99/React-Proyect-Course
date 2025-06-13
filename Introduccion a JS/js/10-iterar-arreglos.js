e159997dde1982b381a8d5a9551b09bae18e0ec7
Angel11R99 <angelrodriguez11r99@gmail.com>
2025-06-13 14:50:36 -0400

    :sparkles: Descomentando un codigo

--- Introduccion a JS/js/10-iterar-arreglos.js
+++ Introduccion a JS/js/10-iterar-arreglos.js
@@ -1,10 +1,9 @@
 const tecnologiaActual = ['Python', 'React', 'JS', 'Tailwind', 'CSS', 'HTML', 'SASS']
 
 // Forma tradicional
-
-// for (let i = 0; i < tecnologiaActual.length; i++) {
-//     console.log(tecnologiaActual[i]);
-// }
+for (let i = 0; i < tecnologiaActual.length; i++) {
+    console.log(tecnologiaActual[i]);
+}
 
 // forEach (no genera un arreglo nuevo)
 tecnologiaActual.forEach(function (techfor) {
