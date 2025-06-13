010b05861767478b8eed85c39dd7bb956a7a8e40
Angel11R99 <angelrodriguez11r99@gmail.com>
2025-06-13 15:35:47 -0400

    :package: Subiendo solo un archivo  dentro de los ejercicios

---
+++ Introduccion a JS/ejercicios/vs.js
@@ -0,0 +1,33 @@
+const estudiante1 = {
+    nombre: 'Jose',
+    presente: 'Presente'
+}
+
+const estudiante2 = {
+    nombre: 'Angel',
+    presente: 'Ausente'
+}
+
+const estudiante3 = {
+    nombre: 'Juan',
+    presente: 'Presente'
+}
+
+// Array con los objetos
+const asistencia = [{ ...estudiante1 }, { ...estudiante2 }, { ...estudiante3 }]
+
+// console.table(asistencia);
+
+// Lista los estudiantes y muestra si esta presentes o ausentes
+const litarAsistencia = asistencia.forEach(function (asis) {
+    console.log(`el estudiante ${asis.nombre} esta presente en estado: ${asis.presente}`);
+})
+
+asistencia.map(function (numAsis) {
+    if (numAsis.presente === 'Presente') {
+        console.log(numAsis.presente);
+        
+    }
+})
+
+// function buscarEstudiante () 
