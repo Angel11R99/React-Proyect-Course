a520e53834182c6b4d80a080571088d5e2378d17
Angel11R99 <angelrodriguez11r99@gmail.com>
2025-06-14 16:50:21 -0400

    :package: export funtion

--- Introduccion a JS/js/11-function-declaration.js
+++ Introduccion a JS/js/11-function-declaration.js
@@ -7,7 +7,24 @@ Nota:
 - Parametros por default `valor1 = 0, valor2 = 0` 
 */
 function sumar (valor1 = 0, valor2 = 0) {
-    console.log(valor1 + valor2);
+    return valor1 + valor2
 }
 
-sumar(12, 10)
+// Export a las funciones para el recurso 18-exportar funciones.js
+// Aplicamos un exporto para exportar una function y uzarla en otro lugar.
+
+/* 
+seguido en al etiquta script ponemos type="module" 
+    <script type="module" src="js/11-function-declaration.js"></script>
+*/
+export function multiplicar(n1 = 0, n2 = 0) {
+    return n1 + n2
+}
+
+/*
+Export default. Nota: solo puede haber uno por archivo
+*/
+
+export default function dividir (n1, n2) {
+    return n1 + n2
+}
---
+++ Introduccion a JS/js/18-exportar-funciones.js
@@ -0,0 +1,3 @@
+import { multiplicar as funcionMult } from "./11-function-declaration.js";
+
+console.log(funcionMult(12, 12))
