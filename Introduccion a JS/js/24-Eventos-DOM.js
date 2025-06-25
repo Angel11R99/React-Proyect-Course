a7cf2d9d41b604e286d967c93c1517733632845d
Angel11R99 <angelrodriguez11r99@gmail.com>
2025-06-25 09:49:13 -0400

    :package: Manipulacion de los eventos dentro del DOM

---
+++ Introduccion a JS/js/24-Eventos-DOM.js
@@ -0,0 +1,19 @@
+// Eventos del DOM - Clicks
+// Url para ver eventos https://developer.mozilla.org/es/docs/Web/API/Element
+
+const heading = document.querySelector('.heading')
+
+// heading.addEventListener('click', () => {
+//     heading.textContent = 'Hola Mundo'
+// })
+
+const enlaces = document.querySelectorAll('.navegacion a')
+
+enlaces.forEach((enlace) => {
+    enlace.addEventListener(('click'), (e) => {
+        e.preventDefault()
+        e.target.textContent = 'Diste Click'
+        
+        // console.log(`Diste click`)
+    })
+})
