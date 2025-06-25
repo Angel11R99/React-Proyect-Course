2558f2b348d2fcf48fc7bf03d770d708b79b0742
Angel11R99 <angelrodriguez11r99@gmail.com>
2025-06-25 13:15:50 -0400

    :sparkles: Manera diferente de recorrer los datos

--- Introduccion a JS/js/24-Eventos-DOM.js
+++ Introduccion a JS/js/24-Eventos-DOM.js
@@ -9,11 +9,15 @@ const heading = document.querySelector('.heading')
 
 const enlaces = document.querySelectorAll('.navegacion a')
 
-enlaces.forEach((enlace) => {
-    enlace.addEventListener(('click'), (e) => {
-        e.preventDefault()
-        e.target.textContent = 'Diste Click'
-        
-        // console.log(`Diste click`)
-    })
-})
+// enlaces.forEach((enlace) => {
+//     enlace.addEventListener(('click'), (e) => {
+//         e.preventDefault()
+//         e.target.textContent = 'Diste Click'
+//         // console.log(`Diste click`)
+//     })
+// })
+
+// Forma distinta de hacerlo
+enlaces.forEach(enlace=>enlace.addEventListener("click",()=>{
+    enlace.textContent="Diste click"
+}))
